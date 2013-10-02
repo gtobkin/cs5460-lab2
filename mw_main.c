acb21a7e1c54f1d78f3be4da7acb831c67694760
gtobkin <greg.tobkin@gmail.com>
2013-10-01 17:36:58 -0700

    Add main C file + comment plan in main + API files.

---
+++ mw_main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include "mw_api.h"
+
+int main (int argc, char **argv) {
+	
+	/*
+		TODO:
+
+		1) Instantiate a mw_api_spec struct
+		2) Populate it
+		3) Run MW_Run(&struct)
+		That's it, actually
+	
+		For our sample problem, we'll do Gauss:
+		read in a (reasonably-sized) integer n from args,
+		return sum of 1 through n, actually by adding them up
+		one-by-one. Note: THIS IS A REALLY DUMB WAY TO ADD THEM.
+	*/
+
+
+	exit(0);
+}
